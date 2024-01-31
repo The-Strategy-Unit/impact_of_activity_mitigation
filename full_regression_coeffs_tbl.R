80dd8d57bb14a9b072cc84c6891818a06c749edd
ghobro <gh13177@my.bristol.ac.uk>
2024-01-31 10:22:35 +0000

    We now double the number of tables to 8 as we provide both the parametric and non-parametric coefficients. We print that to a multi-tabular Excel workbook

--- full_regression_coeffs_tbl.R
+++ full_regression_coeffs_tbl.R
@@ -5,19 +5,32 @@ extract_model_coefs <- function(df, df_name) {
                weights = population_share,
                data = df)
   
-  # Extract the coefficients
-  coefs <- broom::tidy(model, parametric = TRUE)
+  # Extract the parametric coefficients
+  coefs_parametric <- broom::tidy(model, parametric = TRUE)
   
   # Filter to the relevant rows and do the manipulations
-  coefs <- coefs |>
+  coefs_parametric <- coefs_parametric |>
     mutate(pod = df_name,
+           parametric = "parametric",
            central_estimate = exp(estimate),
            confidence_interval_lower = exp(estimate - 1.96 * std.error),
            confidence_interval_higher = exp(estimate + 1.96 * std.error),
            p.value = p.value) |>
-    dplyr::select(pod, term, central_estimate, confidence_interval_lower, confidence_interval_higher, p.value)
-
+    dplyr::select(pod, parametric, term, central_estimate, confidence_interval_lower, confidence_interval_higher, p.value)
+  
+  # Extract the non-parametric coefficients
+  coefs_non_parametric <- broom::tidy(model, parametric = FALSE)
+  
+  # Filter to the relevant rows and do the manipulations
+  coefs_non_parametric <- coefs_non_parametric |>
+    mutate(pod = df_name,
+           parametric = "non-parametric") |>
+    dplyr::select(pod, parametric, term, edf, ref.df, p.value)
+  
+  # Outputs a list containing the parametric terms and non-parametric terms with labels
+  return(list(parametric = coefs_parametric, non_parametric = coefs_non_parametric))
 }
+
 
 # Names of the data frames in the list
 df_names <- names(activity_mitigators_gam_data_split)
@@ -25,8 +38,11 @@ df_names <- names(activity_mitigators_gam_data_split)
 # Use map2 from purrr to pass both the data frames and their names
 model_coefs <- map2(activity_mitigators_gam_data_split, df_names, extract_model_coefs)
 
-# Combine all results into a single data frame
-model_coefs <- bind_rows(model_coefs)
+# We now have a list of lists. The higher level list has 4 elements - one for 
+# each pod - and the lower level list contains the two data frames - one for the
+# parametric terms and one for the non-parametric terms. We use the list_flatten
+# function to collapse this into one list of 8 data frames.
+model_coefs <- list_flatten(model_coefs)
 
-# write to csv
-write.csv(model_coefs, file="full_model_coefs.csv")
+# write the tables to a multi-worksheet workbook
+openxlsx::write.xlsx(model_coefs, file = "full_model_coefs.xlsx")
