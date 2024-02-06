32f85e7fdbdf1e9c7746daae827de38f1e625bf0
ghobro <gh13177@my.bristol.ac.uk>
2024-02-06 14:33:53 +0000

    Removes the non-parametric sheets from the workbook and presents the model results without exponentiating them

--- full_regression_coeffs_tbl.R
+++ full_regression_coeffs_tbl.R
@@ -12,23 +12,13 @@ extract_model_coefs <- function(df, df_name) {
   coefs_parametric <- coefs_parametric |>
     mutate(pod = df_name,
            parametric = "parametric",
-           central_estimate = exp(estimate),
-           confidence_interval_lower = exp(estimate - 1.96 * std.error),
-           confidence_interval_higher = exp(estimate + 1.96 * std.error),
+           central_estimate = estimate,
+           confidence_interval_95pc_lower = estimate - 1.96 * std.error,
+           confidence_interval_95pc_higher = estimate + 1.96 * std.error,
            p.value = p.value) |>
-    dplyr::select(pod, parametric, term, central_estimate, confidence_interval_lower, confidence_interval_higher, p.value)
+    dplyr::select(pod, parametric, term, central_estimate, confidence_interval_95pc_lower, confidence_interval_95pc_higher, p.value)
   
-  # Extract the non-parametric coefficients
-  coefs_non_parametric <- broom::tidy(model, parametric = FALSE)
   
-  # Filter to the relevant rows and do the manipulations
-  coefs_non_parametric <- coefs_non_parametric |>
-    mutate(pod = df_name,
-           parametric = "non-parametric") |>
-    dplyr::select(pod, parametric, term, edf, ref.df, p.value)
-  
-  # Outputs a list containing the parametric terms and non-parametric terms with labels
-  return(list(parametric = coefs_parametric, non_parametric = coefs_non_parametric))
 }
 
 
@@ -38,11 +28,6 @@ df_names <- names(activity_mitigators_gam_data_split)
 # Use map2 from purrr to pass both the data frames and their names
 model_coefs <- map2(activity_mitigators_gam_data_split, df_names, extract_model_coefs)
 
-# We now have a list of lists. The higher level list has 4 elements - one for 
-# each pod - and the lower level list contains the two data frames - one for the
-# parametric terms and one for the non-parametric terms. We use the list_flatten
-# function to collapse this into one list of 8 data frames.
-model_coefs <- list_flatten(model_coefs)
 
 # write the tables to a multi-worksheet workbook
 openxlsx::write.xlsx(model_coefs, file = "full_model_coefs.xlsx")
